75ce37f95e8eec97dad20643262b559270f685ad
STROKE-MODS <hs619689@gmail.com>
2026-08-16 01:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (43.64%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int b = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[b] = nums[i];
+                b++;
+            }
+            else{
+                continue;
+            }
+        }
+        return b;
+    }
+}
